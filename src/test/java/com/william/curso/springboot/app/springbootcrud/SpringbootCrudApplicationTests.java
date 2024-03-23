9a0639b75c29d1ee0f610ac360ef4020bc76e739
William Wun <williamwun@gmail.com>
2024-03-23 08:59:57 -0700

    Project initialized - Web API Restful - CRUD s12v1

---
+++ src/test/java/com/william/curso/springboot/app/springbootcrud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.curso.springboot.app.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
